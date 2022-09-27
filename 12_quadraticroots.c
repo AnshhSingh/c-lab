1c1a30a0f4fb515b56dc48085ab45ffd890706e4
Ansh <ANSHSINGH25BD@GMAIL.COM>
2022-09-27 20:19:16 +0530

    Update 12_quadraticroots.c

--- 12_quadraticroots.c
+++ 12_quadraticroots.c
@@ -1,2 +1,30 @@
 //refer https://www.tutorialspoint.com/how-to-write-a-c-program-to-find-the-roots-of-a-quadratic-equation
-// stil looking for a better way to do this
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    float root1, root2;
+    float root_part, denom;
+
+    printf("Enter values of a, b and c\n");
+    scanf("%f%f%f", &a, &b, &c);
+
+    if (a == 0)
+    {
+        printf("enter a valid quadratic eq");
+    }
+    else
+    {
+        root_part = sqrt(b * b - 4 * a * c);
+        denom = 2 * a;
+
+        root1 = (-b + root_part) / denom;
+        root2 = (-b - root_part) / denom;
+
+        printf("Root1 = %f\nRoot2 = %f", root1, root2);
+    }
+
+    return 0;
+}
